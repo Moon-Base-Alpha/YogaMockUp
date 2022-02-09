9e4c6740ae2e49510c300e2da803528702b73cfb
chi.k.larsson@gmail.com <chi.k.larsson@gmail.com>
2022-02-09 08:23:39 +0100

    fix contact page, fix show address, phone, zipcode on userslist, some css,

---
+++ Models/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YogaMockUp.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(100), Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Required, StringLength(100)]
+        public string Email { get; set; }
+
+        [Required, StringLength(500)]
+        public string Subject { get; set; }
+
+        [Required, StringLength(1000)]
+        public string Message { get; set; }
+
+        public Contact()
+        {
+
+        }
+    }
+}
